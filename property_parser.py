93bdbbdf253361ec2d060286540cd81968cad2ea
Siavoosh Payandeh Azad <siavooshpayandeh@gmail.com>
2017-07-13 11:24:38 +0300

    added some testing

--- property_parser.py
+++ property_parser.py
@@ -6,6 +6,7 @@ import sys
 from functions import *
 from package import sys_arguments
 from run_sim import run_simulator
+from testing import *
 
 # TODO: check if everything is ok with the user's system! if there is a library dependency missing etc...
 # TOOD: we need unit testing!
@@ -14,6 +15,7 @@ sys_arguments = copy.deepcopy(parse_arguments(sys.argv, sys_arguments))					# pa
 generate_folders()		# generates folder structure
 prop_cond_dict, prop_symp_dict = generate_prop_dictionary(sys_arguments["input_property_file"])		# parse the property recieved from input file 
 report_prop_dictonary(prop_cond_dict)		# prints contecnt of the the property dictionary to console
+perform_tests(prop_cond_dict,prop_symp_dict)
 generate_tb(sys_arguments["testbench_file"], prop_cond_dict, prop_symp_dict)	# generates the TB lists
 generate_do_file(sys_arguments["testbench_file"], prop_cond_dict)	# generates the do files
 run_simulator(len(prop_cond_dict), sys_arguments["testbench_file"])	# runs the simulator and generates the coverage reports!
---
+++ testing.py
@@ -0,0 +1,42 @@
+# Copyright (C) 2017 Siavoosh Payandeh Azad and Tsotne Putkaradze
+# License: GNU GENERAL PUBLIC LICENSE Version 3
+import sys
+
+
+def test_prop_cond_dict(prop_cond_dict):
+	for item in prop_cond_dict.keys():
+		if isinstance(prop_cond_dict[item], list):
+			for sub_item in prop_cond_dict[item]:
+				if not isinstance(sub_item, str):
+					return False
+		else:
+			return False
+	return True
+
+
+def test_prop_symp_dict(prop_symp_dict):
+	for item in prop_symp_dict.keys():
+		if isinstance(prop_symp_dict[item], list):
+			for sub_item in prop_symp_dict[item]:
+				if not isinstance(sub_item, str):
+					return False
+		else:
+			return False
+	return True
+
+
+def perform_tests(prop_cond_dict,prop_symp_dict):
+	print "----------------------------------------------"
+	print "starting testing..."
+	if not test_prop_cond_dict(prop_cond_dict):
+		print "=>    prop_cond_dict test faild..."
+		sys.exit()
+	else:
+		print "=>    prop_cond_dict test passed..."
+
+	if not test_prop_symp_dict(prop_symp_dict):
+		print "=>    prop_symp_dict test faild..."
+		sys.exit()
+	else:
+		print "=>    prop_symp_dict test passed..."
+	return None
