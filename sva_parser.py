69877b08cd1afe78aee6eb6847252209bc2934c4
Siavoosh Payandeh Azad <siavooshpayandeh@gmail.com>
2017-07-19 20:35:07 +0300

    bug fix in the parser regarding nextstate_184

--- sva_parser.py
+++ sva_parser.py
@@ -2,6 +2,16 @@
 # License: GNU GENERAL PUBLIC LICENSE Version 3
 
 from math import log
+import sys
+
+
+def clean_string(line_string):
+	new_string = ""
+	for char in line_string:
+		if char != "(" and char != ")" and char != "\n" and char != "\r" and char != " ":
+			new_string += char
+	return new_string
+
 
 def generate_prop_dictionary_sva(prop_file_name):
 
@@ -14,60 +24,67 @@ def generate_prop_dictionary_sva(prop_file_name):
 	counter = 0
 	prop_cond_dict = {}
 	prop_symp_dict = {}
+	start = False
 	for line in prop_file:
+		
 		delay = None
 		repeat_sequence = None
 		current_cycle_prop = None
-		# TODO: fix property nextstate_184;
 		if "endmodule" in line:	# this is the end of usefull part!
 			break
-		if "(rst)" in line:
+		if "    )\n" == line:
+			start = False
+		if start == True:
+			if "##" in line and "|->" not in line: 
+				delay = line[line.index("##")+2:line.index("(")]
+				current_cycle_prop = line[line.index("("):]
+			else:
+				refined_list =[]
+				for item in clean_string(line).split("&&"):
+					if "==" in item:
+						refined_list.append(item[:item.index("=")])
+					else:
+						refined_list.append(item)
+			if "[*" in line:
+				repeat_start = line.index("[*") 
+				repeat_end  = line.index("]")
+				repeat_sequence = line[repeat_start+2:repeat_end]
+				current_cycle_prop = current_cycle_prop[:current_cycle_prop.index("[*")]
+			if current_cycle_prop != None:
+				refined_list = []
+				for item in clean_string(line).split("&&"):
+					if "==" in item:
+						refined_list.append(item[:item.index("=")])
+					else:
+						refined_list.append(item)
+				if repeat_sequence != None:
+					if ":" not in repeat_sequence:
+						for i in range(0, int(repeat_sequence)):
+							dict_delay += int(delay)
+							for item in refined_list: 
+								prop_cond_dict[counter].append(dict_delay*"X"+item)
+					else:
+						repeat = 1
+						if repeat_sequence.split(":")[1] != "$":
+							repeat  = repeat_sequence.split(":")[1]
+						else:
+							pass
+							# TODO: handel $ cases!
+						for i in range(0, int(repeat)):
+							dict_delay += int(delay)
+							for item in refined_list: 
+								prop_cond_dict[counter].append(dict_delay*"X"+item)
+				else:
+					dict_delay += int(delay)
+					for item in refined_list: 
+						prop_cond_dict[counter].append(dict_delay*"X"+item)
+
+		if line == "    (\n" :
+			start = True
 			dict_delay = 0
 			prop_cond_dict[counter] = []
 			delay = 0
 			repeat_sequence = None
-			current_cycle_prop  = "(rst)"
-		if "##" in line and "|->" not in line: 
-			delay = line[line.index("##")+2:line.index("(")]
-			current_cycle_prop = line[line.index("("):]
-		if "[*" in line:
-			repeat_start = line.index("[*") 
-			repeat_end  = line.index("]")
-			repeat_sequence = line[repeat_start+2:repeat_end]
-			current_cycle_prop = current_cycle_prop[:current_cycle_prop.index("[*")]
-		if current_cycle_prop != None:
-			prop = ""
-			for char in current_cycle_prop:
-				if char != "(" and char != ")" and char != "\n" and char != "\r" and char != " ":
-					prop += char
-
-			refined_list = []
-			for item in prop.split("&&"):
-				if "==" in item:
-					refined_list.append(item[:item.index("=")])
-				else:
-					refined_list.append(item)
-			if repeat_sequence != None:
-				if ":" not in repeat_sequence:
-					for i in range(0, int(repeat_sequence)):
-						dict_delay += int(delay)
-						for item in refined_list: 
-							prop_cond_dict[counter].append(dict_delay*"X"+item)
-				else:
-					repeat = 1
-					if repeat_sequence.split(":")[1] != "$":
-						repeat  = repeat_sequence.split(":")[1]
-					else:
-						pass
-						# TODO: handel $ cases!
-					for i in range(0, int(repeat)):
-						dict_delay += int(delay)
-						for item in refined_list: 
-							prop_cond_dict[counter].append(dict_delay*"X"+item)
-			else:
-				dict_delay += int(delay)
-				for item in refined_list: 
-					prop_cond_dict[counter].append(dict_delay*"X"+item)
 
 		if "|->" in line:
 			sub_line = line[line.index(">")+1:]
@@ -85,6 +102,8 @@ def generate_prop_dictionary_sva(prop_file_name):
 	prop_file.close()
 	print "length of condtion dictionary:", len(prop_cond_dict.keys())
 	print "length of symptom dictionary:",len(prop_symp_dict.keys())
+	if len(prop_cond_dict.keys()) != len(prop_symp_dict.keys()):
+		raise ValueError("length of symptom and condition dictionaries are different!")
 	return prop_cond_dict, prop_symp_dict
 
 
